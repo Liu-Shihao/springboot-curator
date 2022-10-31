945e8d3e210798ebf50354f876f63952b323c95c
LiuShihao <liush99@foxmail.com>
2022-10-31 15:55:03 +0800

    Curator Transaction API

---
+++ src/main/java/com/lsh/api/CuratorTransactionApi.java
@@ -0,0 +1,47 @@
+package com.lsh.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.transaction.CuratorOp;
+import org.apache.curator.framework.api.transaction.CuratorTransactionResult;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.util.Collection;
+
+/**
+ * @Author: LiuShihao
+ * @Date: 2022/10/31 14:08
+ * @Desc: Curator 事务 API：
+ * Curator还支持事务，一组crud操作同生同灭
+ */
+@Slf4j
+public class CuratorTransactionApi {
+
+    public static void main(String[] args) throws Exception {
+        String connectionString = "192.168.153.131:2181";
+        ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3,Integer.MAX_VALUE);
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient(connectionString, retryPolicy);
+        curatorFramework.start();
+
+        CuratorOp createParentNode = curatorFramework.transactionOp().create().forPath("/a", "some data".getBytes());// create  /a
+        CuratorOp createChildNode = curatorFramework.transactionOp().create().forPath("/a/path", "other data".getBytes());// create  /a/path
+        CuratorOp setParentNode = curatorFramework.transactionOp().setData().forPath("/a", "other data".getBytes());//setData  /a
+        CuratorOp deleteParent = curatorFramework.transactionOp().delete().forPath("/a"); // delete  /a
+
+        Collection<CuratorTransactionResult> results = curatorFramework.transaction().forOperations(createParentNode, createChildNode, setParentNode,deleteParent);
+
+        for ( CuratorTransactionResult result : results ){
+            System.out.println(result.getForPath() + " - " + result.getType());
+        }
+        /**
+         * 上面的例子最后会抛异常KeeperErrorCode = Directory not empty，由于最后一步delete的节点/a存在子节点，所以整个事务commit失败。
+         *
+         * 注意：事务操作的时候不支持自动创建父节点,也就是说你想创建的节点如果是多层的,那么父节点一定要存在才可以。
+         */
+    }
+
+
+
+
+}
